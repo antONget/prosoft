e10401541e6264e4ad4d41fd2bd15ed0d8c5c53b
antONget <a.l.ponomare@mail.ru>
2024-04-19 11:29:29 +0300

    получить базу данных

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -27,3 +27,6 @@ async def all_message(message: Message) -> None:
     if message.text == '/get_logfile':
         file_path = "py_log.log"
         await message.answer_document(FSInputFile(file_path))
+    if message.text == '/get_dbfile':
+        file_path = "database.db"
+        await message.answer_document(FSInputFile(file_path))
